8795b9a2dd917342a1143b1a18a3e43442053709
Yurii Pakhota <Ov3RforDream@gmail.com>
2022-11-22 16:37:32 +0200

    Play with the directives

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,55 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view />
+  <router-view /> -->
+  <div>
+    <form @submit.prevent>
+      <h4>Create a post</h4>
+      <input v-model="title" class="input" type="text" name="" id="" placeholder="Title">
+      <input v-model="body" class="input" type="text" name="" id="" placeholder="Description">
+      <button class="btn" @click="createPost"></button>
+    </form>
+    <div class="post" v-for="post in posts" v-bind:key="post.id">
+      <div><strong>Название: </strong>{{ post.title }}</div>
+      <div><strong>Описание: </strong>{{ post.body }}</div>
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: 'JavaScript',
+          body: 'JavaScript info',
+        },
+        {
+          id: 2,
+          title: 'TypeScript',
+          body: 'TypeScript info',
+        },
+      ],
+      title: '',
+      body: '',
+    };
+  },
+  methods: {
+    createPost() {
+      const post = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      };
+      this.posts.push(post);
+    },
+  },
+};
+</script>
 
 <style>
 #app {
